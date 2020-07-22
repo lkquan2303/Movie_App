c00ede51a7234fb4a71a929192ca6eebc0437846
Uri <uri@enclave.vn>
2020-07-22 10:06:50 +0700

    Initial Commit

---
+++ iOS_Demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Demo2
+//
+//  Created by Uri on 7/22/20.
+//  Copyright © 2020 Uri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
